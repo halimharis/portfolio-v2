ee34fb2b18fd611b755811a02691b9c69453680d
Halim Haris <halimharis15@gmail.com>
2025-02-24 07:08:39 +0700

    Fix border in work page

--- src/components/WorkPage/WorkItem.tsx
+++ src/components/WorkPage/WorkItem.tsx
@@ -6,7 +6,7 @@ export interface IWorkItem {
 
 function WorkItem({ project, field, year }: IWorkItem) {
   return (
-    <div className="cursor-pointer grid grid-cols-12 w-full font-onest py-8 border-b group border-black text-xl lg:bg-leftToRight-black transition-all duration-300 ease-out hover:text-white max-md:text-base">
+    <div className="cursor-pointer grid grid-cols-12 w-full font-onest py-8 border-b border-neutral-300 group text-xl lg:bg-leftToRight-black transition-all duration-300 ease-out hover:text-white max-md:text-base">
       <span className="group-hover:-translate-x-4 transition-transform duration-500 col-span-6 col-start-2 line-clamp-1">
         {project}
       </span>
--- src/components/WorkPage/WorkList.tsx
+++ src/components/WorkPage/WorkList.tsx
@@ -4,7 +4,7 @@ import WorkItem from "./WorkItem";
 function WorkList() {
   return (
     <div className="flex text-black w-full lg:pt-32 pt-16 flex-col ">
-      <div className="grid grid-cols-12 w-full font-onest py-8 border-b text-sm font-bold border-black ">
+      <div className="grid grid-cols-12 w-full font-onest py-8 border-b text-sm font-bold border-neutral-300">
         <span className="col-span-6 col-start-2 ">PROJECT</span>
         <span className="col-span-3 max-md:hidden">FIELD</span>
         <span className="col-span-1 max-md:col-start-9">YEAR</span>
